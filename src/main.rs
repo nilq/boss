3d2e0355a94b63d32f0556d846cdcc2a5052af87
Niels Nielsen <slein.nroh@gmail.com>
2019-01-02 16:27:53 +0100

    [lexer] Add basic lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod boss;
+
+use self::boss::source::*;
+use self::boss::lexer::*;
+
 fn main() {
-    println!("Hello, world!");
+  let test = r#"
+print: fun (bar: int) {
+  x: int = bar
+}
+  "#;
+
+  let source = Source::from("<main>", test.lines().map(|x| x.into()).collect::<Vec<String>>());
+  let lexer  = Lexer::default(test.chars().collect(), &source);
+
+  let mut tokens = Vec::new();
+
+  for token_result in lexer {
+    if let Ok(token) = token_result {
+      tokens.push(token)
+    } else {
+      return
+    }
+  }
+
+  let mut parser = Parser::new(tokens, &source);
+
+  match parser.parse() {
+    Ok(ref ast) => {
+      println!("{:#?}", ast)
+    },
+
+    _ => ()
+  }
 }
